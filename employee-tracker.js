d5673eb63b5fd8d0e6d328f37fcbcef2dcf8b18a
Coleman Buffa <coleman.buffa@gmail.com>
2021-01-27 23:43:29 -0800

    Add MySQL login credentials and mainMenu function stub.

---
+++ employee-tracker.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  mainMenu();
+});
+
+function mainMenu() {
+	 
+}
